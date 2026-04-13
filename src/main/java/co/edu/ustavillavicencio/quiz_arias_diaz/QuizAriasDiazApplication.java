1890b71d063b156addfb481ea157b9dec66619e8
michael <michaelarias20115@gmail.com>
2026-04-13 10:33:20 -0500

    first commit

---
+++ src/main/java/co/edu/ustavillavicencio/quiz_arias_diaz/QuizAriasDiazApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ustavillavicencio.quiz_arias_diaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAriasDiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAriasDiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ustavillavicencio/quiz_arias_diaz/QuizAriasDiazApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ustavillavicencio.quiz_arias_diaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAriasDiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
